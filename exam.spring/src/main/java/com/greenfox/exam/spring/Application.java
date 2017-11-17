ea1c5fea517e9bacb691fc085dd74a7ea4f299cc
nezih <nezihcihan@hotmail.com>
2017-11-17 10:09:10 +0100

    sql file added to resources and DataSourceConfig class made to map the database

--- exam.spring/src/main/java/com/greenfox/exam/spring/Application.java
+++ exam.spring/src/main/java/com/greenfox/exam/spring/Application.java
@@ -2,11 +2,15 @@ package com.greenfox.exam.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+		SpringApplication springApplication = new SpringApplication();
+		ApplicationContext applicationContext = springApplication.run(Application.class,args);
+
 	}
 }
---
+++ exam.spring/src/main/java/com/greenfox/exam/spring/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package com.greenfox.exam.spring;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
+    @Bean(name = "mainDataSource")
+    public DataSource createMainDataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:"+TEMP_DIRECTORY+"/testdata;MODE=MySQL");
+        return ds;
+    }
+
+}
