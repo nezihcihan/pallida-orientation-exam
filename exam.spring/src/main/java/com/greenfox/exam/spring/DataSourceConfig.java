f8ef6eba336ef3c2349b9413b5eb58abc7e039f6
nezih <nezihcihan@hotmail.com>
2017-11-17 12:32:58 +0100

    html template button added

--- exam.spring/src/main/java/com/greenfox/exam/spring/DataSourceConfig.java
+++ exam.spring/src/main/java/com/greenfox/exam/spring/DataSourceConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 @Configuration
---
+++ exam.spring/src/main/java/com/greenfox/exam/spring/controllers/MainRestController.java
@@ -0,0 +1,9 @@
+package com.greenfox.exam.spring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+
+}
